cc9f0f93ffc53282cc220cf0634690dea85b06f0
kraikov <raykov@weichain.tech>
2019-01-22 13:14:40 +0200

    Remove helper functions from the test main folder.

--- test/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    host: 'http://localhost:3001/',
-    internalHost: 'http://localhost:3001/internal/',
-    weidexSourcePath: './contracts/WeiDex.aes',
-    erc20SourcePath: './contracts/mocks/ERC20.aes',
-    gas: 200000,
-    ttl: 55,
-};
